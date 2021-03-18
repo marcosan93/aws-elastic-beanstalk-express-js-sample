11f08cec2b0955143f8d62db17c17e5ac5c2dba8
marcosan93 <marcosan93@gmail.com>
2021-03-18 15:51:48 -0400

    changed hello world

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello it is working on Sept 29 2020, updated I changed this now'));
+app.get('/', (req, res) => res.send('Hello World! 2021/3/18'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
